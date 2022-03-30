8c52a66ccd10608323910de0e17d127661aa76b8
Anastasia Ryzhova <aruzhova@yandex.ru>
2022-03-31 01:24:13 +0300

    adding my files to my repository

---
+++ js/flipping.js
@@ -0,0 +1,10 @@
+var i=1;
+var image=document.getElementById("image");
+var imgs=new Array('img/malevich.jpg','img/matushin.jpg','img/tatlin_plot.jpg');
+function ImgFlip() {
+    if (image.src.match()){
+        i++;
+        i%=imgs.length;
+    }
+    image.src = imgs[i];
+}
